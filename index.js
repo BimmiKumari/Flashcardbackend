49b6405025fa5f5cbb4c074e62b6c15c9dd1432a
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-08-12 17:31:16 +0530

    All added

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ app.delete('/api/flashcards/:id', (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
