e79d6ef03c3cebb4daf1d28c1c489fbe510a41e9
Bimmi kumari <115577045+BimmiKumari@users.noreply.github.com>
2024-08-12 15:51:19 +0530

    All added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-
+require('dotenv').config();
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Bimmi@123',
-  database: 'flashcards_db'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect();
